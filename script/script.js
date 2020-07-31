e35ddd546878f851eb1df52c24c0cea575273196
website-cloud <63789134+website-cloud@users.noreply.github.com>
2020-07-31 21:40:30 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -6,3 +6,10 @@ menuToggle.onclick = function(){
     menuToggle.classList.toggle('menu-icon-active');
     mobileNavContainer.classList.toggle('mobile-nav--active');
 }
+$(".mail-btn").on("click touchstart", function () {
+    $(this).addClass("fly");
+    that = this
+    setTimeout(function() {
+        $(that).removeClass("fly");
+    }, 5400)
+});
